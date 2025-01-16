ffa6d4d7c8d58008c6fc6c8b24f1c845c2f28eaf
Nicolas Gauthier <nick.habashi@gmail.com>
2025-01-16 11:41:34 -0500

    Remove ignored files from Git cache and refresh .gitignore

--- lib/providers/meals_provider.dart
+++
@@ -1,8 +0,0 @@
-import "package:flutter_riverpod/flutter_riverpod.dart";
-import "package:mealapp/data/dummy_data.dart";
-
-final mealsProvider = Provider(
-  (ref) {
-    return dummyMeals;
-  },
-);
